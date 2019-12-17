5383521464f1f2efe2f68d97e26dae4874460743
ARashad96 <58879111+ARashad96@users.noreply.github.com>
2019-12-17 19:55:29 +0200

    Add files via upload

---
+++ Slider_Bar.java
@@ -0,0 +1,73 @@
+package com.arashad96.androiddeveloperintermidatekit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class Slider_Bar extends AppCompatActivity {
+    Button github;
+    Button info;
+    SeekBar slider;
+    TextView number;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_slider__bar);
+
+        number = findViewById(R.id.number);
+        slider = findViewById(R.id.slider);
+        slider.setMax(10);
+        slider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                //moving the slider changing the values
+                number.setText(""+i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        github = findViewById(R.id.github);
+        github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/ARashad96/Slider_Bar"));
+                startActivity(intent);
+            }
+        });
+        info = findViewById(R.id.info);
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new android.app.AlertDialog.Builder(Slider_Bar.this)
+                        .setIcon(R.drawable.profile)
+                        .setTitle("App info")
+                        .setMessage("This app is changing number with the slide bar using textview, seekbar and linearlayout.")
+                        .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        })
+                        .show();
+            }
+        });
+    }
+}
